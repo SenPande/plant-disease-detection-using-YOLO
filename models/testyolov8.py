627326f550d459cb3c11fcde847e03f4a4ede952
JiashuoC <jisho.cheng@gmail.com>
2026-03-27 15:26:56 +0100

    Fix: Path risultati

--- models/testyolov8.py
+++ models/testyolov8.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
 import torch
+import os
 
 def main():
-    device = '0' if torch.cuda.is_available() else 'cpu'
-    print(f"Utilizzando: {torch.cuda.get_device_name(int(device))}")
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Utilizzando: {torch.cuda.get_device_name(device)}")
 
     model = YOLO('yolov8s.pt')
 
@@ -11,10 +12,10 @@ def main():
         data = 'data/test/data.yaml',
         epochs = 100,
         imgsz = 512,
-        batch = 8,
+        batch = 16,
         workers = 2,
         device = device,
-        project = 'models/train_results',
+        project = os.path.join(os.getcwd(), 'models/train_results'),
         name = 'Test_ExG_KAGGLE',
         plots = True,
         cache = False
